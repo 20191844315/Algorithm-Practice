b231a234a459c030b6feffa9b03f598719fabe21
weaker <1574341302@qq.com>
2025-05-15 16:55:55 +0800

    Update 485.最大连续-1-的个数.cpp

--- 题解/双指针/485.最大连续-1-的个数.cpp
+++ 题解/双指针/485.最大连续-1-的个数.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem485.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=485 lang=cpp
  *
